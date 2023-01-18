f101fddd8ab2880e893925f075316c70a8d0070b
Ryan Mansfield <ryan.mansfield@launchpadlab.com>
2023-01-18 18:28:51 -0500

    Use pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # get 'todos/index'
+  # get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "todos#index"
+  root "pages#home"
 end
