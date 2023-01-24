f5e9ea4f818b5fefe23ad24688e98b0ddda96bf6
Ryan Mansfield <ryan.mansfield@launchpadlab.com>
2023-01-24 12:22:44 -0500

    Remove Beers Controller, Add API Beers controller and routes

--- app/views/beers/_beer.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! beer, :id, :brand, :style, :country, :quanity, :created_at, :updated_at
-json.url beer_url(beer, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :beers
+  namespace :api do
+    namespace :v1 do
+      get 'beers/index'
+      post 'beers/create'
+      delete 'beers/:id', to: 'beers#destroy'
+    end
+  end
   # get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
